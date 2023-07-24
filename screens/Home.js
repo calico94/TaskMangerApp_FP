3873ecfe36fc0f2307509ed090d07cf708401b52
calico94 <amybkchan@gmail.com>
2023-07-25 00:55:47 +0800

    Add add button in Home.js

--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,6 @@
 import React, { useContext, useState, useEffect} from 'react';
-import { View, StyleSheet, Keyboard, TouchableWithoutFeedback} from 'react-native';
+import { View, StyleSheet, Keyboard, TouchableWithoutFeedback,
+TouchableOpacity, Text } from 'react-native';
 import TaskList from '../components/Tasks/TaskList';
 
 import { TasksContext } from '../contexts/TasksContext';
@@ -9,13 +10,24 @@ const Home = () => {
   
   return (
       <TouchableWithoutFeedback
-        style={{backgroundColor:'red'}} onPress={() => {
+        onPress={() => {
         Keyboard.dismiss();
         }}>
       <View style={styles.container}>
 
         {/* Task List Component */}
         <TaskList tasks={tasks}/>
+
+        {/* Add Button */}
+        <View style={styles.addButton.container}>
+          <TouchableOpacity
+            style={
+              styles.addButton.button
+            }
+          >
+            <Text style={styles.addButton.text}>+</Text>
+          </TouchableOpacity>
+        </View>
 
       </View>
       
@@ -28,6 +40,26 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
   },
+  addButton: {
+    text: {
+      color: 'white',
+      fontSize: 32,
+    },
+    container: {
+      flex: 1,
+      justifyContent: 'flex-end',
+      alignItems: 'center',
+    },
+    button:{
+      marginBottom: 20,
+      backgroundColor: '#34eb83',
+      width: 64,
+      height: 64,
+      borderRadius: 32,
+      alignItems: 'center',
+      justifyContent: 'center',
+    }, 
+  }, 
 });
   
 export default Home;
